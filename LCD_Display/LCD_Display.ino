8d2c6a10d8c42fa95230b45998a56c56864840fb
Nooshin Pourkamali <nooshin.pourkamali@gmail.com>
2025-11-22 16:17:04 +0100

    Add files via upload

---
+++ LCD_Display/LCD_Display.ino
@@ -0,0 +1,16 @@
+#include <LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  
+  lcd.begin(16, 2); 
+  lcd.setCursor(0, 0); 
+  lcd.print("Hello World."); 
+  lcd.setCursor(0, 1); 
+  lcd.print("Can you see me?");
+
+
+void loop() {
+}
